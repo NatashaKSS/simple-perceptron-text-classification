002f360cf8afb6a98e43ad77831838bf3858583e
NatashaKSS <xnagol@gmail.com>
2017-11-08 17:20:04 +0800

    Added basic tokenizer functionality

--- Tokenizer.py
+++ Tokenizer.py
@@ -15,3 +15,25 @@ class Tokenizer():
 
   def stem(self, word):
     return self.PorterStemmer.stem(word, 0, len(word) - 1)
+
+  #===========================================================================#
+  # STRING MANIPULATION FUNCS
+  #===========================================================================#
+
+  """
+  Split on 1st whitespace from back
+  """
+  def split_on_whitespace_from_back(self, input_str):
+    return input_str.rsplit(' ', 1)
+
+  """
+  Split on 1st '/' from back
+  """
+  def split_on_slash_from_back(self, input_str):
+    return input_str.rsplit('/', 1)
+
+  """
+  Trim newline char from an input string
+  """
+  def strip_newline(self, input_str):
+    return input_str.strip('\n')
